cd1bc0b03e30ac855040bd42845f7dce9f015d11
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2023-06-18 15:51:11 +0100

    Added uk and bg local time variables

--- light-dark-mode-digital-clock/src/App.jsx
+++ light-dark-mode-digital-clock/src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const UKTime = new Date().getTime()
+  const localOffset = new Date().getTimezoneOffset()*60000
+  console.log(localOffset);
+  const utc = UKTime + localOffset
+  const bgOffset = +3
+  const bg = utc + (3600000 * bgOffset)
+
+  const [time,setTime] = useState(new Date().toLocaleTimeString())
+  const [bgTime, setBgTime] = useState(new Date(bg).toLocaleTimeString())
+  const [date,setDate] = useState(new Date().toLocaleDateString())
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='light'>
+      <h3>UK time</h3>
+      <p>{time}</p>
+      <h3>BG time</h3>
+      <p>{bgTime}</p>
+    </div>
   )
 }
 
