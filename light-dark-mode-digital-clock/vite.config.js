76f2a7a50f24114d741f8f5fa6c08328b2b01da4
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2023-09-14 14:25:27 +0100

    Vite base added

--- light-dark-mode-digital-clock/vite.config.js
+++ light-dark-mode-digital-clock/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Light-dark-mode-digital-clock/light-dark-mode-digital-clock",
+  base: "./Light-dark-mode-digital-clock/light-dark-mode-digital-clock",
   plugins: [react()],
 })
